b1307b44b9bb8052ad13692f30ddb0b3e9bb9ed3
Aaron Sumner <asumner@mac.com>
2013-01-17 23:38:21 -0600

    add drill-down view of month in detail

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -18,6 +18,10 @@ class PurchasesController < ApplicationController
       f.title(text: 'Royalties by month')
       f.xAxis(type: :datetime, categories: @dates)
       f.yAxis(min: 0, title: { text: "Royalties"} )
+      f.options[:plotOptions][:column] = {
+        cursor: 'pointer',
+        point: { events: { click: %|function() { location.href = '/purchases/' + this.category }|.js_code } }
+      }
     end
 
     @sales_chart = LazyHighCharts::HighChart.new('graph', style: '') do |f|
@@ -27,6 +31,10 @@ class PurchasesController < ApplicationController
       f.title(text: 'Sales by month')
       f.xAxis(type: :datetime, categories: @dates)
       f.yAxis(min: 0, title: { text: "Sales"} )
+      f.options[:plotOptions][:column] = {
+        cursor: 'pointer',
+        point: { events: { click: %|function() { location.href = '/purchases/' + this.category }|.js_code } }
+      }
     end
 
     @average_chart = LazyHighCharts::HighChart.new('graph', style: '') do |f|
@@ -35,6 +43,50 @@ class PurchasesController < ApplicationController
       f.series(name: 'Royalty', data: @averages)
       f.title(text: 'Average royalty by month')
       f.xAxis(type: :datetime, categories: @dates)
+      f.yAxis(min: 0, title: { text: "Royalty per book"} )
+      f.options[:plotOptions][:line] = {
+        cursor: 'pointer',
+        point: { events: { click: %|function() { location.href = '/purchases/' + this.category }|.js_code } }
+      }
+    end
+  end
+
+  def show
+    @purchases = Purchase.
+      select("purchased_on, COUNT(id) AS total_sales, SUM(royalty) AS royalty, created_at").
+      where("STRFTIME('%Y-%m', purchased_on) = ?", params[:id]).
+      order("purchased_on ASC").
+      group("purchased_on")
+
+    @dates = @purchases.map(&:purchased_on)
+    @royalties = @purchases.map(&:royalty)
+    @sales = @purchases.map(&:total_sales)
+    @averages = @purchases.map {|p| p.royalty/p.total_sales }
+
+    @royalties_chart = LazyHighCharts::HighChart.new('graph', style: '') do |f|
+      f.options[:chart][:defaultSeriesType] = 'column'
+      f.legend(enabled: false)
+      f.series(name: 'Royalties', data: @royalties)
+      f.title(text: 'Royalties by date')
+      f.xAxis(type: :datetime, categories: @dates, labels: { rotation: 90, align: 'left' })
+      f.yAxis(min: 0, title: { text: "Royalties"} )
+    end
+
+    @sales_chart = LazyHighCharts::HighChart.new('graph', style: '') do |f|
+      f.options[:chart][:defaultSeriesType] = 'column'
+      f.legend(enabled: false)
+      f.series(name: 'Sales', data: @sales)
+      f.title(text: 'Sales by date')
+      f.xAxis(type: :datetime, categories: @dates, labels: { rotation: 90, align: 'left' })
+      f.yAxis(min: 0, title: { text: "Sales"} )
+    end
+
+    @average_chart = LazyHighCharts::HighChart.new('graph', style: '') do |f|
+      f.options[:chart][:defaultSeriesType] = 'line'
+      f.legend(enabled: false)
+      f.series(name: 'Royalty', data: @averages)
+      f.title(text: 'Average royalty by date')
+      f.xAxis(type: :datetime, categories: @dates, labels: { rotation: 90, align: 'left' })
       f.yAxis(min: 0, title: { text: "Royalty per book"} )
     end
   end
