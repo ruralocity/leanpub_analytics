9c2b52c242bd071e594caab7761d25838a7eabb9
Aaron Sumner <asumner@mac.com>
2013-05-14 03:25:41 -0500

    account for changes to leanpub's csv output

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -7,21 +7,33 @@ class Purchase < ActiveRecord::Base
   MAPPINGS = {
     "Bundle slug" => "bundle_slug",
     "Purchase ID" => "leanpub_id",
-    "Total Paid" => "total_paid",
-    "Your Royalty" => "royalty",
-    "Purchased at" => "purchased_on",
-    "Paid out at" => "paid_on",
-    "Coupon code" => "coupon_code",
-    "Coupon_note" => "coupon_note",
-    "Email" => "email"
+    "Total Paid for Book" => "total_paid",
+    "Total Book Royalty" => "royalty",
+    "Date Purchased" => "purchased_on",
+    "Date Author Royalty Was Paid" => "paid_on",
+    "Coupon Code" => "coupon_code",
+    "Coupon Note" => "coupon_note",
+    "Email If Shared" => "email"
   }
+
+  def self.most_recent
+    Purchase.last.try(:created_at)
+  end
 
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
       purchase = find_by_leanpub_id(row["Purchase ID"]) || new
+      row["Total Paid for Book"] = dollars_to_f(row["Total Paid for Book"])
+      row["Total Book Royalty"] = dollars_to_f(row["Total Book Royalty"])
       purchase.attributes = Hash[row.to_hash.map {|k,v|[MAPPINGS[k], v]}].slice(*accessible_attributes)
       # raise purchase.attributes.inspect
       purchase.save!
     end
+  end
+
+  private
+
+  def self.dollars_to_f(amount)
+    amount.delete('$,').to_f
   end
 end
